544b8b5e07234f787f35b36c191c39543e6716ed
Dillon Fagan <dillonfagan@pm.me>
2021-02-21 00:44:06 -0500

    add GET /

--- src/muck.cr
+++ src/muck.cr
@@ -1,6 +1,14 @@
-# TODO: Write documentation for `Muck`
-module Muck
-  VERSION = "0.1.0"
+require "kemal"
+require "json"
 
-  # TODO: Put your code here
+store = Hash(String, String).new
+
+store["username"] = "bob"
+store["password"] = "Pa$$w0rd"
+
+get "/" do |env|
+  env.response.content_type = "application/json"
+  store.to_json
 end
+
+Kemal.run
