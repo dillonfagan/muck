dc22c5d96a719660bbb06925b49e83861ea8c4f0
Dillon Fagan <dillonfagan@pm.me>
2021-02-21 17:40:38 -0500

    rename stash to store

--- src/muck.cr
+++ src/muck.cr
@@ -15,37 +15,37 @@ get "/" do |env|
   stash.stores.to_json
 end
 
-get "/:stash" do |env|
-  stash_name = env.params.url["stash"]
-  stash.in(stash_name).get_all.to_json
+get "/:store" do |env|
+  store = env.params.url["store"]
+  stash.in(store).get_all.to_json
 end
 
-get "/:stash/:key" do |env|
-  stash_name = env.params.url["stash"]
+get "/:store/:key" do |env|
+  store = env.params.url["store"]
   key = env.params.url["key"]
-  stash.in(stash_name).get(key).to_json
+  stash.in(store).get(key).to_json
 end
 
-put "/:stash" do |env|
-  stash_name = env.params.url["stash"]
+put "/:store" do |env|
+  store = env.params.url["store"]
   entry = Entry.from_json env.request.body.not_nil!
-  stash.in(stash_name).add(entry).to_json
+  stash.in(store).add(entry).to_json
 end
 
-patch "/:stash/:key" do |env|
-  stash_name = env.params.url["stash"]
+patch "/:store/:key" do |env|
+  store = env.params.url["store"]
   key = env.params.url["key"]
   value = env.params.json["value"].as(String)
 
-  before = stash.in(stash_name).update key, value
-  after = stash.in(stash_name).get key
+  before = stash.in(store).update key, value
+  after = stash.in(store).get key
   Message::Patch.new(before, after).to_json
 end
 
-delete "/:stash/:key" do |env|
-  stash_name = env.params.url["stash"]
+delete "/:store/:key" do |env|
+  store = env.params.url["store"]
   key = env.params.url["key"]
-  entry = stash.in(stash_name).delete key
+  entry = stash.in(store).delete key
   Message::Delete.new(entry).to_json
 end
 
